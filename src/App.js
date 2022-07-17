ba4bda0e09a60bc69c350b7ab53057dae359c6ca
Irizkyw <ichwanrizkywahyudin3@gmail.com>
2022-07-17 19:19:24 +0700

    rubah app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component,useState } from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import "./App.css";
+import AddUsers from "./Pages/AddUsers";
 import Home from "./Pages/Home";
 import Users from "./Pages/Users";
 
@@ -18,11 +19,15 @@ class App extends Component {
             <li>
               <Link to="/users">Users</Link>
             </li>
+            <li>
+              <Link to="/users/add">Tambah User</Link>
+            </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<Users />} />
+          <Route path="/users/add" element={<AddUsers />}/>
           <Route path="*" element={<div>404</div>} />
         </Routes>
       </Router>
---
+++ src/Pages/AddUsers.js
@@ -0,0 +1,24 @@
+import React, { Component,Form } from "react";
+
+export default class AddUsers extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Form>
+          <label>Username</label>
+          <input type="text" name="username"></input>
+          <label>Email</label>
+          <input type="text" name="email"></input>
+          <label>Password</label>
+          <input type="text" name="password"></input>
+          <label>Roles_id</label>
+          <input type="text" name="roles_id"></input>
+          <button type="submit" name="submit">
+            Tambahkan
+          </button>
+        </Form> */}
+        tambah
+      </div>
+    );
+  }
+}
